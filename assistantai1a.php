0de7640232ed8faa96ef645400a7179f9360ad52
jogit64 <usersfdc18@gmail.com>
2024-05-01 13:08:15 +0200

    main qui se charge bien là où code court!

--- assistantai1a.php
+++ assistantai1a.php
@@ -20,36 +20,44 @@ class Assistant1a_Widget extends WP_Widget
         );
     }
 
-    // Affichage du widget
     public function widget($args, $instance)
     {
-        echo $args['before_widget'];
-        if (!empty($instance['title'])) {
-            echo $args['before_title'] . apply_filters('widget_title', $instance['title']) . $args['after_title'];
+        // Vérifier si le shortcode [assistant1a] est présent dans le contenu de la page
+        if (is_singular() && has_shortcode(get_post_field('post_content', get_the_ID()), 'assistant1a')) {
+            echo $args['before_widget'];
+            if (!empty($instance['title'])) {
+                echo $args['before_title'] . apply_filters('widget_title', $instance['title']) . $args['after_title'];
+            }
+            echo $args['after_widget'];
         }
-        echo $args['after_widget'];
     }
 }
 
 // Enregistre les styles et les scripts du plugin.
-function assistant1a_enqueue_styles()
+function assistant1a_enqueue_styles_and_scripts()
 {
-    wp_enqueue_style('assistant1a-style', plugins_url('css/assistant-ai-1a-style.css', __FILE__));
-    wp_enqueue_style('dashboard-style', plugins_url('css/dashboard-style.css', __FILE__));
-    wp_enqueue_script('assistant1a-script', plugins_url('js/assistant-ai-1a.js', __FILE__), array('jquery'), false, true);
-    // Dans ton fichier PHP où tu enregistres ton script
-    wp_enqueue_script('dashboard-script', plugins_url('js/dashboard.js', __FILE__), array('jquery'), false, true);
-    wp_localize_script('dashboard-script', 'dashboardSettings', array(
-        'baseUrl' => plugins_url('/', __FILE__)
-    ));
+    // Vérifier si le shortcode [assistant1a] est présent dans le contenu de la page
+    global $post;
+    if (is_a($post, 'WP_Post') && has_shortcode($post->post_content, 'assistant1a')) {
+        // Le shortcode est présent, charger le CSS et le script JavaScript
+        wp_enqueue_style('assistant1a-style', plugins_url('css/assistant-ai-1a-style.css', __FILE__));
+        wp_enqueue_style('dashboard-style', plugins_url('css/dashboard-style.css', __FILE__));
 
-    wp_enqueue_script('pdfjs', 'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.7.570/pdf.min.js', array(), null, true);
-    wp_enqueue_script('pdfjs-worker', 'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.7.570/pdf.worker.min.js', array(), null, true);
-    wp_enqueue_script('jspdf', 'https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.3.1/jspdf.umd.min.js', array(), null, true);
-    wp_enqueue_script('html2canvas', 'https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.3.2/html2canvas.min.js', array(), null, true);
+        wp_enqueue_script('assistant1a-script', plugins_url('js/assistant-ai-1a.js', __FILE__), array('jquery'), false, true);
+        wp_enqueue_script('dashboard-script', plugins_url('js/dashboard.js', __FILE__), array('jquery'), false, true);
+        wp_localize_script('dashboard-script', 'dashboardSettings', array(
+            'baseUrl' => plugins_url('/', __FILE__)
+        ));
+
+        wp_enqueue_script('pdfjs', 'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.7.570/pdf.min.js', array(), null, true);
+        wp_enqueue_script('pdfjs-worker', 'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.7.570/pdf.worker.min.js', array(), null, true);
+        wp_enqueue_script('jspdf', 'https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.3.1/jspdf.umd.min.js', array(), null, true);
+        wp_enqueue_script('html2canvas', 'https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.3.2/html2canvas.min.js', array(), null, true);
+    }
 }
 
-add_action('wp_enqueue_scripts', 'assistant1a_enqueue_styles');
+add_action('wp_enqueue_scripts', 'assistant1a_enqueue_styles_and_scripts');
+
 
 
 
@@ -106,136 +114,134 @@ function assistant1a_shortcode()
     ob_start(); // Commence la capture de sortie
     // HTML du formulaire
 ?>
-<div class="widget">
+    <div class="widget">
 
 
-    <form id="assistant1a-form" enctype="multipart/form-data" method="post">
+        <form id="assistant1a-form" enctype="multipart/form-data" method="post">
 
-        <div class="zone-containRadio">
+            <div class="zone-containRadio">
 
-            <fieldset>
-                <legend>Usages</legend>
-                <div class="zone-radio">
-                    <label><input type="radio" name="config" value="salarie" checked> Entretien salarié</label><br>
-                    <label><input type="radio" name="config" value="direction"> Entretien direction</label><br>
-                    <!-- <label><input type="radio" name="config" value="mp3"> Transcription audio</label><br> -->
-                    <label><input type="radio" name="config" value="document"> Analyse documentaire</label><br>
-                    <label><input type="radio" name="config" value="discussion"> Rédaction</label><br>
+                <fieldset>
+                    <legend>Usages</legend>
+                    <div class="zone-radio">
+                        <label><input type="radio" name="config" value="salarie" checked> Entretien salarié</label><br>
+                        <label><input type="radio" name="config" value="direction"> Entretien direction</label><br>
+                        <!-- <label><input type="radio" name="config" value="mp3"> Transcription audio</label><br> -->
+                        <label><input type="radio" name="config" value="document"> Analyse documentaire</label><br>
+                        <label><input type="radio" name="config" value="discussion"> Rédaction</label><br>
 
-                </div>
-                <div id="instructionText"></div>
-            </fieldset>
-
-        </div>
-        <div class="zone-ctrlFIcSess">
-
-            <fieldset>
-                <legend>Gestion du fichier</legend>
-                <div id="assistant1a-file-section">
-                    <input type="file" id="assistant1a-file" name="file" accept=".doc,.docx,.pdf,.ppt,.pptx,.txt">
-                    <button type="button" id="assistant1a-file-submit" class="custom-button">Envoyer le fichier</button>
-                </div>
-                <!-- Insertion du fieldset de l'estimation des coûts ici pour un affichage en ligne -->
-                <fieldset id="cost-estimate-fieldset">
-                    <legend>Estimation des coûts</legend>
-                    <div id="cost-estimate-container">
-                        <div class="details-container">
-                            <div class="token-details">
-                                <h3>Tokens</h3>
-                                <p>Input: <span id="token-input">0</span></p>
-                                <p>Output: <span id="token-output">0</span></p>
-                                <p>Total: <span id="token-total">0</span></p>
-                            </div>
-                            <div class="cost-details">
-                                <h3>Coût</h3>
-                                <p><span id="cost-estimated">0.00</span></p>
-                            </div>
-                            <div class="quality-details">
-                                <h3>Qualité</h3>
-                                <p><span id="quality">N/A</span></p>
-                                <!-- Ajout d'un élément pour afficher le nombre maximum de tokens -->
-                                <p id="max-tokens-display" style="font-size: 0.8em; color: #666;">Max tokens: Non défini
-                                </p>
-                                <div id="token-progress-bar"
-                                    style="width: 100%; background-color: #ddd; height: 24px; margin-top: 5px;">
-                                    <div id="token-progress"
-                                        style="height: 100%; width: 0%; background-color: #4CAF50;"></div>
-                                </div>
-                                <p id="token-status">Statut des
-                                    tokens non disponible</p>
-                            </div>
-
-
-                        </div>
                     </div>
+                    <div id="instructionText"></div>
                 </fieldset>
-            </fieldset>
+
+            </div>
+            <div class="zone-ctrlFIcSess">
+
+                <fieldset>
+                    <legend>Gestion du fichier</legend>
+                    <div id="assistant1a-file-section">
+                        <input type="file" id="assistant1a-file" name="file" accept=".doc,.docx,.pdf,.ppt,.pptx,.txt">
+                        <button type="button" id="assistant1a-file-submit" class="custom-button">Envoyer le fichier</button>
+                    </div>
+                    <!-- Insertion du fieldset de l'estimation des coûts ici pour un affichage en ligne -->
+                    <fieldset id="cost-estimate-fieldset">
+                        <legend>Estimation des coûts</legend>
+                        <div id="cost-estimate-container">
+                            <div class="details-container">
+                                <div class="token-details">
+                                    <h3>Tokens</h3>
+                                    <p>Input: <span id="token-input">0</span></p>
+                                    <p>Output: <span id="token-output">0</span></p>
+                                    <p>Total: <span id="token-total">0</span></p>
+                                </div>
+                                <div class="cost-details">
+                                    <h3>Coût</h3>
+                                    <p><span id="cost-estimated">0.00</span></p>
+                                </div>
+                                <div class="quality-details">
+                                    <h3>Qualité</h3>
+                                    <p><span id="quality">N/A</span></p>
+                                    <!-- Ajout d'un élément pour afficher le nombre maximum de tokens -->
+                                    <p id="max-tokens-display" style="font-size: 0.8em; color: #666;">Max tokens: Non défini
+                                    </p>
+                                    <div id="token-progress-bar" style="width: 100%; background-color: #ddd; height: 24px; margin-top: 5px;">
+                                        <div id="token-progress" style="height: 100%; width: 0%; background-color: #4CAF50;"></div>
+                                    </div>
+                                    <p id="token-status">Statut des
+                                        tokens non disponible</p>
+                                </div>
 
 
-            <fieldset>
-                <legend>Session utilisateur</legend>
-                <!-- <div id="assistant1a-session-section"> -->
-                <button type="button" id="assistant1a-reset" class="custom-button">Réinitialiser la Session</button>
-                <!-- </div> -->
-            </fieldset>
-        </div>
+                            </div>
+                        </div>
+                    </fieldset>
+                </fieldset>
 
 
-        <div class="zone-scrib">
-            <!-- <input type="text" id="assistant1a-question" name="question" placeholder="Posez votre question ici..."> -->
-            <button type="button" id="assistant1a-record" class="custom-button">
-                <img src="<?php echo plugins_url('assets/micro.png', __FILE__); ?>" alt="Micro">
-            </button>
-            <button type="button" id="assistant1a-stop" class="custom-button" style="display:none;">Arrêter</button>
-            <textarea id="assistant1a-question" name="question" placeholder="Posez votre question ici..."></textarea>
+                <fieldset>
+                    <legend>Session utilisateur</legend>
+                    <!-- <div id="assistant1a-session-section"> -->
+                    <button type="button" id="assistant1a-reset" class="custom-button">Réinitialiser la Session</button>
+                    <!-- </div> -->
+                </fieldset>
+            </div>
 
-            <!-- <button type="button" id="assistant1a-submit" class="custom-button">Demander</button> -->
-            <button type="button" id="assistant1a-submit" class="custom-button">
-                <img src="<?php echo plugins_url('assets/sortie.png', __FILE__); ?>" alt="Demander">
-            </button>
-        </div>
-        <!-- <div id="zone-session">
+
+            <div class="zone-scrib">
+                <!-- <input type="text" id="assistant1a-question" name="question" placeholder="Posez votre question ici..."> -->
+                <button type="button" id="assistant1a-record" class="custom-button">
+                    <img src="<?php echo plugins_url('assets/micro.png', __FILE__); ?>" alt="Micro">
+                </button>
+                <button type="button" id="assistant1a-stop" class="custom-button" style="display:none;">Arrêter</button>
+                <textarea id="assistant1a-question" name="question" placeholder="Posez votre question ici..."></textarea>
+
+                <!-- <button type="button" id="assistant1a-submit" class="custom-button">Demander</button> -->
+                <button type="button" id="assistant1a-submit" class="custom-button">
+                    <img src="<?php echo plugins_url('assets/sortie.png', __FILE__); ?>" alt="Demander">
+                </button>
+            </div>
+            <!-- <div id="zone-session">
                 <fieldset>
                     <legend>Session utilisateur</legend>
                     <button type="button" id="assistant1a-reset" class="custom-button">Réinitialiser la Session</button>
                 </fieldset>
             </div> -->
 
-        <div class="charger">
+            <div class="charger">
 
-            <div id="loadingMessage" class="loading-message"></div>
-            <div id="staticMessage" class="static-message">(En cas de problème lors du traitement, un message sera
-                affiché ici.)</div>
+                <div id="loadingMessage" class="loading-message"></div>
+                <div id="staticMessage" class="static-message">(En cas de problème lors du traitement, un message sera
+                    affiché ici.)</div>
 
-            <div id="assistant1a-file-upload-status" style="display:none;">
-                <div class="loader">
-                    <div class="bar"></div>
-                    <div class="bar"></div>
-                    <div class="bar"></div>
+                <div id="assistant1a-file-upload-status" style="display:none;">
+                    <div class="loader">
+                        <div class="bar"></div>
+                        <div class="bar"></div>
+                        <div class="bar"></div>
+                    </div>
+
                 </div>
+            </div>
+
+            <div id="assistant1a-response"></div>
+
+            <div id="response-actions" style="display:none;">
+                <!-- Actions cachées par défaut -->
+                <button id="copyButton">Copier l'échange</button>
+                <button id="saveButton">Sauvegarder l'échange</button>
+
+                <label>
+                    <input type="checkbox" id="toggleHistoryCheckbox" unchecked>
+                    Afficher l'historique
+                </label>
 
             </div>
-        </div>
-
-        <div id="assistant1a-response"></div>
-
-        <div id="response-actions" style="display:none;">
-            <!-- Actions cachées par défaut -->
-            <button id="copyButton">Copier l'échange</button>
-            <button id="saveButton">Sauvegarder l'échange</button>
-
-            <label>
-                <input type="checkbox" id="toggleHistoryCheckbox" unchecked>
-                Afficher l'historique
-            </label>
-
-        </div>
 
 
-    </form>
+        </form>
 
-    <div id="assistant1a-history"></div>
-</div>
+        <div id="assistant1a-history"></div>
+    </div>
 
 
 <?php
